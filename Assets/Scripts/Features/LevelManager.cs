71eb869f194e3b1175de75cd3042da3cbd7393b6
Raphael Mello <128510237+raphael-mello-dev@users.noreply.github.com>
2025-07-02 10:47:55 -0300

    Objects color change

--- Assets/Scripts/Features/LevelManager.cs
+++ Assets/Scripts/Features/LevelManager.cs
@@ -11,20 +11,34 @@ public class LevelManager : MonoBehaviour
     [Range(2, 10)]
     [SerializeField] private int levelSize;
 
+    [SerializeField] Color[] colors;
+
     private void Start()
     {
-        Instantiate(levelsPool[0], Environment.transform);
 
-        int lastIndex = 0;
-        int currentIndex = 0;
+        int lastIndex = 0, currentIndex = 0;
+        int floorColorIndex, borderColorIndex;
+        GameObject LevelPiece;
 
         for (int i = 0; i < levelSize - 1; i++)
         {
             while (lastIndex == currentIndex)
                 currentIndex = (int) Mathf.Floor(Random.Range(0, levelsPool.Count));
 
-            GameObject LevelPiece = Instantiate(levelsPool[currentIndex], Environment.transform);
+            if (i == 0)
+                LevelPiece = Instantiate(levelsPool[0], Environment.transform);
+            else
+                LevelPiece = Instantiate(levelsPool[currentIndex], Environment.transform);
             
+            floorColorIndex = Random.Range(0, colors.Length - 1);
+            LevelPiece.transform.GetChild(0).GetComponent<Renderer>().material.color = colors[floorColorIndex];
+
+            do { borderColorIndex = Random.Range(0, colors.Length - 1); }
+            while (floorColorIndex == borderColorIndex);
+
+            LevelPiece.transform.GetChild(1).GetComponent<Renderer>().material.color = colors[borderColorIndex];
+            LevelPiece.transform.GetChild(2).GetComponent<Renderer>().material.color = colors[borderColorIndex];
+
             if (i == levelSize - 2)
             {
                 Transform finishLine = LevelPiece.transform.GetChild(LevelPiece.transform.childCount - 1);
@@ -32,7 +46,7 @@ public class LevelManager : MonoBehaviour
             }
 
             LevelPiece.transform.position = new Vector3(LevelPiece.transform.position.x, LevelPiece.transform.position.y, 
-                LevelPiece.gameObject.transform.position.z + (i + 1) * 170);
+                LevelPiece.gameObject.transform.position.z + i * 169);
 
             //LevelPiece.SetActive(false);
             lastIndex = currentIndex;
