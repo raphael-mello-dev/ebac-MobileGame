9ab6e0ef8b8dddf7cc91611e69050dd48f08b944
Raphael Mello <128510237+raphael-mello-dev@users.noreply.github.com>
2025-07-06 12:56:51 -0300

    Player scale on picking power-up

--- Assets/Scripts/Powerups/PowerupBase.cs
+++ Assets/Scripts/Powerups/PowerupBase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
 
 public abstract class PowerupBase: MonoBehaviour
 {
@@ -14,11 +15,21 @@ public abstract class PowerupBase: MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(PlayerScale(other.gameObject));
             StartCoroutine(Powerup(other.gameObject));
+        }
     }
 
     public virtual IEnumerator Powerup(GameObject player)
     {
         yield return null;
+    }
+
+    private IEnumerator PlayerScale(GameObject player)
+    {
+        player.transform.GetChild(player.transform.childCount - 1).DOScale(1.2f, 0.2f).SetEase(Ease.InBounce);
+        yield return new WaitForSecondsRealtime(0.2f);
+        player.transform.GetChild(player.transform.childCount - 1).DOScale(1f, 0.2f).SetEase(Ease.InBounce);
     }
 }
